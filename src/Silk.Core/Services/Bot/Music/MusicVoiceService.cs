503b98fb0ad16e3f6ee0dfa692b0db34c193520a
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-07-20 08:16:24 -0400

    Initial commit

---
+++ src/Silk.Core/Services/Bot/Music/MusicVoiceService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus.VoiceNext;
+using Microsoft.Extensions.Hosting;
+
+namespace Silk.Core.Services.Bot.Music
+{
+	/// <summary>
+	/// A service responsible for music playback in voice channels.
+	/// </summary>
+	public sealed class MusicVoiceService : IHostedService
+	{
+		private readonly ConcurrentDictionary<ulong, VoiceNextConnection> _voiceConnections;
+		private readonly ConcurrentDictionary<ulong, SemaphoreSlim> _semaphores;
+		public async Task StartAsync(CancellationToken cancellationToken) { }
+		public async Task StopAsync(CancellationToken cancellationToken) { }
+
+		public async Task Play() { }
+		public async Task Stop() { }
+		public async Task Pause() { }
+		public async Task Repeat() { }
+		public async Task Shuffle() { }
+		
+	}
+}
