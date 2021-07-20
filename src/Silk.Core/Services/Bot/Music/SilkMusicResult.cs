fab47675c9920e2f4dc4380de7e5870ddff8ef8e
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-07-20 10:42:39 -0400

    Add Video property to MusicResult

--- src/Silk.Core/Services/Bot/Music/SilkMusicResult.cs
+++ src/Silk.Core/Services/Bot/Music/SilkMusicResult.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.IO;
 using DSharpPlus.Entities;
+using YoutubeExplode.Videos;
 
 namespace Silk.Core.Services.Bot.Music
 {
 	public sealed class SilkMusicResult
 	{
+		public Video Video { get; init; }
 		public DiscordUser RequestedBy { get; init; }
 		public TimeSpan Duration { get; init; }
 		
